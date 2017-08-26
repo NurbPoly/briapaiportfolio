7a52eac271538c2e5af465d7f960cc930b244e3c
BriannaP <pbriann1@gmail.com>
2017-08-26 15:30:05 -0400

    Add files via upload

--- random-quote-machine.js
+++ random-quote-machine.js
@@ -1,5 +1,6 @@
 
 
+/*** Twitter API ***/
 // Embedded Tweet button/ From twitter's dev resources
 window.twttr = (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0],
@@ -19,3 +20,20 @@ window.twttr = (function(d, s, id) {
 }(document, "script", "twitter-wjs"));
 
 
+
+/**** Test Quotes w/o API & w/ Vanilla JS ****/
+
+var quotes = [
+'Blah blah blahblahblahblahblah blah blah blah blah 1',
+'Blah blahblahblahblah blahblahblahblah blahblahblahblah blahblahblahblah blahblahblahblah  blahblahblahblah blahblahblahblah blahblahblahblah 2',
+'Blah blahblahblahblah blah blahblah blahblahblahblah blahblahblahblah blahblahblahblah blahblahblahblah  blahblah 3',
+'Blahblahblah blahblahblahblahblahblahblahblahblah blah blah 4',
+'Blahblah blahblah blahblah blahblahblahblahblahblah  blahblahblahblah  blahblah 5',
+'Blah blahblahblahblahblahblahblahblah blahblahblahblah blahblahblahblah blahblahblahblah blahblahblahblah  blahblah 6'
+]
+
+function newQuote() {
+    var randomNumber = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('quote-box').innerHTML = quotes[randomNumber];
+}
+/*** End of test quotes ***/
